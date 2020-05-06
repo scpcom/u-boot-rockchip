80efc23f0430b75965554136dd1b275fcc77d5ca
scpcom <scpcom@gmx.de>
2020-05-06 23:14:33 +0200

    . u-boot-odroidn1-loaders

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ make
 
 tools/rk_tools/bin/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
 
-tools/mkimage -n rk3399 -T rksd -d tools/rk_tools/bin/rk33/rk3399_ddr_800MHz_v1.08.bin idbloader.img
-cat tools/rk_tools/bin/rk33/rk3399_miniloader_v1.06.bin >> idbloader.img
+tools/mkimage -n rk3399 -T rksd -d ../rkbin/bin/rk33/rk3399_ddr_800MHz_v1.15.bin idbloader.img
+cat ../rkbin/bin/rk33/rk3399_miniloader_v1.15.bin >> idbloader.img
 
-cp tools/rk_tools/bin/rk33/rk3399_loader_v1.08.106.bin sd_fuse
+cp ../rkbin/bin/rk33/rk3399_loader_v1.12.112.bin sd_fuse
 
 tools/rk_tools/bin/trust_merger tools/rk_tools/trust.ini
 
