7cf424c33de9653f4c45d225c6545ea5ecbd5431
scpcom <scpcom@gmx.de>
2020-05-06 23:14:30 +0200

    next-dev rockchip-rk3288_common-remove_fdt_high_initrd_high_to_enable_relocation_skip_by_android_image_separate

--- include/configs/rk3288_common.h
+++ include/configs/rk3288_common.h
@@ -68,8 +68,6 @@
 #include <config_distro_bootcmd.h>
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"fdt_high=0x0fffffff\0" \
-	"initrd_high=0x0fffffff\0" \
 	"fdtfile=" FDTFILE \
 	"partitions=" PARTS_DEFAULT \
 	ENV_MEM_LAYOUT_SETTINGS \
