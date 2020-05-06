fbe45e66bd034b969ab496705509ac1ec42b8c68
scpcom <scpcom@gmx.de>
2020-05-06 23:13:52 +0200

    next-dev rockchip-temp-revert-rk3328-board-spl

--- arch/arm/mach-rockchip/rk3328-board-spl.c
+++ arch/arm/mach-rockchip/rk3328-board-spl.c
@@ -11,49 +11,16 @@
 #include <ram.h>
 #include <spl.h>
 #include <asm/io.h>
-#include <asm/gpio.h>
-#include <asm/arch/bootrom.h>
 #include <asm/arch/cru_rk3328.h>
 #include <asm/arch/grf_rk3328.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/periph.h>
 #include <asm/arch/timer.h>
-#include <dt-bindings/gpio/gpio.h>
-#include <dt-bindings/pinctrl/rockchip.h>
-
-#define GRF_BASE		0xFF100000
-#define GPIO0_BASE  0xFF210000
 
 DECLARE_GLOBAL_DATA_PTR;
 
 void board_debug_uart_init(void)
 {
-}
-
-void board_init_sdmmc_pwr_en(void)
-{
-	struct rk3328_grf_regs * const grf = (void *)GRF_BASE;
-	struct rockchip_gpio_regs * const gpio0 = (void *)GPIO0_BASE;
-
-	printf("board_init_sdmmc_pwr_en\n");
-
-	rk_clrsetreg(&grf->com_iomux,
-		IOMUX_SEL_SDMMC_MASK,
-		IOMUX_SEL_SDMMC_M1 << IOMUX_SEL_SDMMC_SHIFT);
-
-	rk_clrsetreg(&grf->gpio0d_iomux,
-		GPIO0D6_SEL_MASK,
-		GPIO0D6_GPIO << GPIO0D6_SEL_SHIFT);
-
-	rk_clrsetreg(&grf->gpio1a_iomux,
-		GPIO1A0_SEL_MASK,
-		GPIO1A0_CARD_DATA_CLK_CMD_DETN
-		<< GPIO1A0_SEL_SHIFT);
-
-	// set GPIO0_D6 to GPIO_ACTIVE_LOW
-	// set GPIO0_D6 to OUTPUT
-	clrbits_le32(&gpio0->swport_dr, 1 << RK_PD6);
-	setbits_le32(&gpio0->swport_ddr, 1 << RK_PD6);
 }
 
 void board_init_f(ulong dummy)
@@ -68,7 +35,6 @@ void board_init_f(ulong dummy)
 	}
 
 	preloader_console_init();
-	board_init_sdmmc_pwr_en();
 
 	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
 	if (ret) {
@@ -84,26 +50,7 @@ u32 spl_boot_mode(const u32 boot_device)
 
 u32 spl_boot_device(void)
 {
-	u32 bootdevice_brom_id = readl(RK3328_BROM_BOOTSOURCE_ID_ADDR);
-	switch (bootdevice_brom_id) {
-		case BROM_BOOTSOURCE_EMMC:
-			printf("booted from eMMC\n");
-			return BOOT_DEVICE_MMC1;
-
-		case BROM_BOOTSOURCE_SD:
-			printf("booted from SD\n");
-			return BOOT_DEVICE_MMC2;
-
-		case BROM_BOOTSOURCE_SPINOR:
-			printf("booted from SPI flash\n");
-			return BOOT_DEVICE_SPI;
-
-		case BROM_BOOTSOURCE_USB:
-			printf("booted from USB\n");
-			return BOOT_DEVICE_MMC1;
-	}
-
-	return BOOT_DEVICE_BOOTROM;
+	return BOOT_DEVICE_MMC1;
 }
 
 #ifdef CONFIG_SPL_LOAD_FIT
