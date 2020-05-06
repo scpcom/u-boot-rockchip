f284172995a478ecbb2d7368570f6177720c73a0
scpcom <scpcom@gmx.de>
2020-05-06 23:15:05 +0200

    build.sh: use open source tools

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ make odroidn1_defconfig
 
 make
 
-tools/rk_tools/bin/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
+tools/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
 
 tools/mkimage -n rk3399 -T rksd -d ../rkbin/bin/rk33/rk3399_ddr_800MHz_v1.15.bin idbloader.img
 cat ../rkbin/bin/rk33/rk3399_miniloader_v1.15.bin >> idbloader.img
 
 cp ../rkbin/bin/rk33/rk3399_loader_v1.12.112.bin sd_fuse
 
-tools/rk_tools/bin/trust_merger tools/rk_tools/trust.ini
+tools/trust_merger ${BIN_PATH_FIXUP} tools/rk_tools/trust.ini
 
 cp idbloader.img sd_fuse
 cp uboot.img sd_fuse
