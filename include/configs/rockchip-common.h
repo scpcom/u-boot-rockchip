4a79ec8020c99411b44475667b06be5bf8f7ca15
scpcom <scpcom@gmx.de>
2020-05-08 04:40:52 +0200

    next-dev rockchip-rockchip-common-fix

--- include/configs/rockchip-common.h
+++ include/configs/rockchip-common.h
@@ -144,20 +144,9 @@
 #define RKIMG_BOOTCOMMAND			\
 	"boot_android ${devtype} ${devnum};"
 #else
-/*
- * Update images a/b and active slot with fastboot
- * when avb+ab system boot failed.
- * Remove rockusb since it unable to active slot.
- */
-#define RKIMG_BOOTCOMMAND \
-	"boot_android ${devtype} ${devnum};" \
-	"echo AVB boot failed and enter fastboot!;" \
-	"fastboot usb 0;"
-#endif /* CONFIG_ANDROID_AB */
-#else /* CONFIG_AVB_VBMETA_PUBLIC_KEY_VALIDATE */
-#define RKIMG_BOOTCOMMAND \
-	"run distro_bootcmd;" \
-	"boot_android ${devtype} ${devnum};" \
+#define RKIMG_BOOTCOMMAND			\
+	"run distro_bootcmd;"			\
+	"boot_android ${devtype} ${devnum};"	\
 	"bootrkp;"
 #endif
 
